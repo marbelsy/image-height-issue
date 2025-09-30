9ba5ead12372641dca04fef30c8be930c5835c9e
mBaldasty <markus.baldasty@offizium.de>
2025-09-30 20:48:27 +0200

    commit 2

--- app/components/NewsTeaserInline.vue
+++ app/components/NewsTeaserInline.vue
@@ -1,9 +1,10 @@
 <template>
-  <GridLayout columns="*, 105" marginBottom="10">
+  <GridLayout columns="*, 105" rows="auto, *" marginBottom="10">
     <GridLayout rows="auto, auto"
                 columns="auto, auto, *, 105"
                 col="0"
                 colSpan="2"
+                row="1"
     >
       <HTMLLabel :text="item.dateDisplayValue" row="0" col="0" marginLeft="15"/>
       <GridLayout row="1" col="0" colSpan="4"
@@ -18,7 +19,9 @@
         <HTMLLabel :text="item.news_title" maxLines="1" row="1"/>
       </GridLayout>
     </GridLayout>
-    <NSImg row="0" col="1" stretch="aspectFill" :src="item.imageSource"/>
+    <NSImg row="0" col="1"
+           rowSpan="2"
+           stretch="aspectFill" :src="item.imageSource"/>
   </GridLayout>
 </template>
 
